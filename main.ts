5ea1c7848f9c0325cdd0e0a4507b404379702edf
Yoshiya Hinosawa <stibium121@gmail.com>
2023-08-17 17:20:19 +0900

    update

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ Deno.serve(async () => {
   await kv.set(["1"], 1)
   await kv.set(["2"], 2)
   await kv.set(["3"], 3)
+  await kv.set(["4"], 4)
   await kv.set(["branch-1"], true)
   return new Response("hi");
 });
