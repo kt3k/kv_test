4676d9f1313cef3428604b475d3495425fa6f827
Yoshiya Hinosawa <stibium121@gmail.com>
2023-08-17 17:14:38 +0900

    update

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ const kv = await Deno.openKv();
 
 Deno.serve(async () => {
   await kv.set(["1"], 1)
-  await kv.set(["main"], true)
+  await kv.set(["feat/this-branch-has-very-very-long-name"], true)
   return new Response("hi");
 });
