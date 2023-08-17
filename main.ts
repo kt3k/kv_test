7d02bd1ed0e11473a147683153726f6ba01b4d69
Yoshiya Hinosawa <stibium121@gmail.com>
2023-08-17 17:10:25 +0900

    update

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ const kv = await Deno.openKv();
 Deno.serve(async () => {
   await kv.set(["1"], 1)
   await kv.set(["2"], 2)
+  await kv.set(["3"], 3)
   await kv.set(["branch-1"], true)
   return new Response("hi");
 });
